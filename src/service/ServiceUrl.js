a5bb2c7c4ba119da8851a08cb6e4f71e5139f934
Naveen <129765472+Navii02@users.noreply.github.com>
2025-02-15 09:00:52 +0530

    Update ServiceUrl.js

--- src/service/ServiceUrl.js
+++ src/service/ServiceUrl.js
@@ -1,3 +1,3 @@
-//export const url="https://localshoppebackend.onrender.com"
+export const url="https://localshoppebackend.onrender.com"
 
-export const url="http://localhost:4000"
+//export const url="http://localhost:4000"
